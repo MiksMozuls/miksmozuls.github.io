5e2424385607b9ae3bf51008d2c2dafa6f5c332a
MiksMozuls <miks.mozuls@gmail.com>
2020-11-26 01:33:19 +0200

    Stylised 404 page

--- Error/notfound.php
+++ Error/notfound.php
@@ -1,1 +1,63 @@
-YOU'VE FUCKED UP
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>My website</title>
+	<script src="./lib/jquery/jquery.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script src="./lib/bootstrap/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="./css/stylesheet.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="./css/reset.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" lang="en">
+	<link rel="stylesheet" href="./css/stylesheet.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	  <a class="navbar-brand" href="#">Erratum</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	  <span class="navbar-toggler-icon"></span>
+	  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Previous work
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="previouswork1.php">1</a>
+          <a class="dropdown-item" href="previouswork2.php">2</a>
+          <a class="dropdown-item" href="previouswork3.php">3</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="previouswork4.php">Video</a>
+        </div>
+      	</li>
+   		<li class="nav-item">
+        	<a class="nav-link" href="contacts.php">Contacts</a>
+      	</li>
+	
+	<li class="nav-item">
+        	<a class="nav-link" href="spa.php">SPA</a>
+      	</li>
+    </ul>
+  </div>
+</nav>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<img class="img-fluid" src="./img/404.jpg" alt="Image not loaded">	
+			</div>  
+		</div>
+		
+		
+	</div>
+
+
+</body> 
+</html>
